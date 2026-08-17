e056626a794e47952148f467068a7ac8635602f8
AaronKuriyan <aaronkuriyan04.com>
2026-08-17 21:14:18 +0530

    Updation

---
+++ ml_models/train.py
@@ -0,0 +1,13 @@
+﻿"""
+Training script for ML models
+"""
+
+from fake_news_classifier import FakeNewsClassifier
+
+def main():
+    print("Starting model training...")
+    classifier = FakeNewsClassifier()
+    print("Models ready for training!")
+
+if __name__ == "__main__":
+    main()
