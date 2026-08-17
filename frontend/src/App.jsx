646c02fcc0aded8c7daf41f102b73e7541bf5a7b
AaronKuriyan <aaronkuriyan04.com>
2026-08-17 21:13:19 +0530

    Updation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Factora - Fake News Detector</h1>
+      <p>Welcome to Factora! Analyze articles for credibility.</p>
+    </div>
+  );
+}
+
+export default App;
